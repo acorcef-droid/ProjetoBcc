8c6bde0adb1289a7d789d36b76e83f4a9db67628
Acorce <acorcef@gmail.com>
2026-05-05 13:03:39 -0300

    algumas alterações

--- projetobcc/main.py
+++ projetobcc/main.py
@@ -9,7 +9,8 @@ app = ctk.CTk()
 app.title("Convertor para Audio 2.0 👍")
 app.geometry("800x400")
 
-label = ctk.CTkLabel(app, text="Bem-Vindo ao convertor de texto 👉 audio", font=("Helvetica", 20))
+label = ctk.CTkLabel(
+    app, text="Bem-Vindo ao convertor de texto 👉 audio", font=("Helvetica", 20))
 label.pack(pady=20)
 
 entry = ctk.CTkEntry(
@@ -24,8 +25,10 @@ entry.pack(pady=10, padx=10)
 
 idioma = ctk.StringVar(value="off")
 
+
 def checkbox_event():
     print(f"Checkbox is {idioma.get()}")
+
 
 checkbox = ctk.CTkCheckBox(
     master=app,
@@ -37,19 +40,22 @@ checkbox = ctk.CTkCheckBox(
 )
 checkbox.pack(pady=10)
 
-#função para disparar a conversao:
+# função para disparar a conversao:
+
+
 def disparar_conversao():
     texto_usuario = entry.get()
     idioma_selecionado = idioma.get()
 
     if texto_usuario:
         if idioma_selecionado == "off":
-            idioma_selecionado = "pt" #lingua padrão caso n selecione nada
-        converter(texto_usuario, idioma_selecionado,'meu_audio')
+            idioma_selecionado = "pt"  # lingua padrão caso n selecione nada
+        converter(texto_usuario, idioma_selecionado, 'meu_audio')
 
-get_button = ctk.CTkButton(master=app, text="Converter", command=disparar_conversao)
-get_button.pack(pady=5)
 
+botao_converter = ctk.CTkButton(
+    master=app, text="Converter", command=disparar_conversao)
+botao_converter.pack(pady=5)
 
 
 app.mainloop()
